f79c566f7f2e4bc4465a9e7fded5d75a5824301e
gladwin2802 <ajgladwin2802@gmail.com>
2024-04-28 11:08:27 +0530

    Add .gitignore to exclude specified files and directories

--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start cmd /k "cd C:/Users/ajjos/OneDrive/Desktop/ImageUpload/frontend && npm run dev"
-start cmd /k "cd C:/Users/ajjos/OneDrive/Desktop/ImageUpload/backend && npm start"
-start cmd /k "cd C:/Users/ajjos/OneDrive/Desktop/ImageUpload/backend/pythonBackend && python main.py"
